e0082aca6a77cca7b57e549a1f5d0688f9871eef
sakship1 <88310435+sakship1@users.noreply.github.com>
2021-08-02 14:30:55 +0530

    Initial commit of math.py

---
+++ math.py
@@ -0,0 +1,16 @@
+def add(x,y):
+    return x+y
+def subtract(x,y):
+    return x-y #on master branch
+def multiply(x,y):
+    return x*y
+def divide(x,y):
+    if y==0:    #on master
+        return div_by_0_error
+    else:
+        return x/y
+
+#square
+def square(x):
+    return x*x
+
